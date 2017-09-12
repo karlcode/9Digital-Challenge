420b63a79b03bc0a2ade4a7ea175ac244727af5b
karlcode <karl.l1@hotmail.com>
2017-09-12 20:01:44 +1000

    asda

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-var pg = require('pg');
-
+var bodyParser = require('body-parser')
 
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(function(req,res,next){
+  res.locals.response = null;
+  res.locals.userValue = null;
+  next();
+})
+app.use(function(err, req, res, next) {
+  if (err.status === 400) {
+    res.status(400).json({
+      "error": "Could not decode request: JSON parsing failed"
+    });
+  } else {
+    res.status(err.status).send(err.message);
+  }
+});
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.post('/', function(request, response) {
-  console.log(request)
-
-});
-
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
+app.get('/', function(req, res){ 
+  res.render('pages/db', {
+    userValue: "yolo"
   });
 });
+
+app.post('/', function(req, res) {
+  if (req.body.payload){
+    var requestArray = req.body.payload
+    var responseArray = { response: [] }
+      for (var i = 0; i < requestArray.length; i++){
+        var payload = requestArray[i]
+        if (payload.drm == true && payload.episodeCount > 0){
+          responseArray.response.push({
+            image: payload.image.showImage,
+            slug: payload.slug,
+            title: payload.title
+          })
+        }
+      }
+      
+      res.render('pages/db', { userValue: 'Tobi' }, function(err, html){
+        res.send(html) 
+      })
+      
+  }
+  else {
+    res.json({
+      "error": "Request needs a payload key"
+    })
+  }
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
